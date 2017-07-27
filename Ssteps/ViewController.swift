5526e4dd4c81f8558651debb05cb9773e1102a09
lgentile06 <egentile@umich.edu>
2017-07-27 15:47:22 -0700

    first commit yo

---
+++ Ssteps/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Ssteps
+//
+//  Created by Elizabeth Gentile on 7/27/17.
+//  Copyright © 2017 Elizabeth Gentile. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self as? CLLocationManagerDelegate
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as! CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+    }
+}
+
